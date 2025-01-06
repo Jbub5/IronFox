0f6be35c603c986a720ae6a2e388958d6e29ead4
celenity <celenity@celenity.dev>
2025-01-06 00:34:37 -0500

    enhancement: create patch to remove tracking params from urls
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -321,6 +321,9 @@ patch -p1 --no-backup-if-mismatch --quiet <"$patches/enable-fingerprinting-prote
 # Disable Fakespot ("Shopping Experience"...)
 patch -p1 --no-backup-if-mismatch --quiet <"$patches/disable-shopping-experience.patch"
 
+# Remove tracking parameters from URLs
+patch -p1 --no-backup-if-mismatch --quiet <"$patches/remove-tracking-params.patch"
+
 # Switch the built-in extension recommendations page to use our collection instead of Mozilla's...
 patch -p1 --no-backup-if-mismatch --quiet <"$patches/extension-recommendations.patch"
 
